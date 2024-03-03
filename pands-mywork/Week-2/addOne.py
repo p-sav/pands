0a9794c6c5c73050ed379057d5c9c2c6b898e3ce
p-sav <g00438898@atu.ie>
2024-03-03 23:26:21 +0000

    folder change

---
+++ pands-mywork/Week-2/addOne.py
@@ -0,0 +1,7 @@
+# addOne
+# Program that reads a number and prints out on more than that number
+# author: psav
+
+number = int(input('please enter a number'))
+newNumber = number + 1
+print (f'{number} plus one is {newNumber}')
