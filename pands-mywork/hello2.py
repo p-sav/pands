76b0874490acbbe96b53e7014085923bce09f984
p-sav <g00438898@atu.ie>
2024-03-03 23:10:36 +0000

    new file

---
+++ pands-mywork/hello2.py
@@ -0,0 +1,8 @@
+# hello 2 
+# program that reads in a persons name and prints out 'hello' the result
+# author: p-sav
+
+name = input("Enter your name:")
+print('Hello ' + name + '\nNice to meet you')
+
+            
