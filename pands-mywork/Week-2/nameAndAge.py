102d35df0157b19c6442dbce005ce33ef6788f92
p-sav <g00438898@atu.ie>
2024-03-03 23:50:14 +0000

    week-2

---
+++ pands-mywork/Week-2/nameAndAge.py
@@ -0,0 +1,6 @@
+# nameAndAge
+# Program that reads in a name and age and outputs 'hello andrew, your age is 21' 
+# author p-sav
+
+name = input ("Enter your name ")
+print('hello ' + name + '\nyour age is 21')
