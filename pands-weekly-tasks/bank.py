d1a41dc19efa8cf56e69f10e1c974324f8931a11
p-sav <g00438898@atu.ie>
2024-03-04 21:31:54 +0000

    weekly task 2

---
+++ pands-weekly-tasks/bank.py
@@ -0,0 +1,11 @@
+# bank.py
+# Program that prompts a user and adds two money amounts 
+# author: p-sav
+
+
+num1 = input ("Enter amount 1 (in cent) ")
+num2 = input ("Enter amount 2 (in cent) ")
+sum = (num1 * num2)
+print (sum)
+
+
