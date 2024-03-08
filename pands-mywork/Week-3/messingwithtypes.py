88a605ca685bb74ef78f02b3a4d332d97c7b9300
p-sav <g00438898@atu.ie>
2024-03-08 23:04:37 +0000

    week-3

---
+++ pands-mywork/Week-3/messingwithtypes.py
@@ -0,0 +1,7 @@
+# messing with variable types
+#Author: p-sav
+
+answer_of_question = "afsfs"
+
+print ("answer is " + str(answer_of_question))
+print (f"type of answer is {type(answer_of_question)}")
