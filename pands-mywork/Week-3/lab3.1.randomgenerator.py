94708b1db6bda0d35d1ab78a31186da8aba622b2
p-sav <g00438898@atu.ie>
2024-03-10 16:30:12 +0000

    week-3

---
+++ pands-mywork/Week-3/lab3.1.randomgenerator.py
@@ -0,0 +1,8 @@
+# lab3.1.randomgenerator.py
+# program that generates a random number between 1 & 10 
+# author: p-sav]
+
+import random
+number = random.randint(1,10)
+print("Here is a random number {}".format(number))
+
