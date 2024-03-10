8ce9f336e4fb3b07acbad12b87a1866bbb63d41e
p-sav <g00438898@atu.ie>
2024-03-10 16:45:04 +0000

    week-3

--- pands-mywork/Week-3/lab3.1.randomgenerator.py
+++ pands-mywork/Week-3/lab3.1.randomgenerator.py
@@ -1,8 +1,25 @@
 # lab3.1.randomgenerator.py
-# program that generates a random number between 1 & 10 
+# program that generates a random number 
 # author: p-sav]
 
+'''
 import random
 number = random.randint(1,10)
 print("Here is a random number {}".format(number))
+'''
+
+import random
+
+def generate_random_number(lower_bound, upper_bound):
+    return random.randint(lower_bound, upper_bound)
+
+if __name__ == "__main__":
+    lower_bound = int(input("Enter the lower bound of the range: "))
+    upper_bound = int(input("Enter the upper bound of the range: "))
+
+    if lower_bound >= upper_bound:
+        print("Error: Lower bound must be less than upper bound.")
+    else:
+        random_number = generate_random_number(lower_bound, upper_bound)
+        print("Random number between", lower_bound, "and", upper_bound, ":", random_number)
 
