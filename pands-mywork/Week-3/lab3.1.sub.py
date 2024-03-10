7058a05ec14c6ebbc390e68cd5b8e8e16b3f004d
p-sav <g00438898@atu.ie>
2024-03-10 16:07:05 +0000

    week-3

---
+++ pands-mywork/Week-3/lab3.1.sub.py
@@ -0,0 +1,8 @@
+# lab3.1.sub.py
+# program that reads in two numbers and subtracts one number from another
+# author: p-sav
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+answer = x-y
+print("{} minus {} is {} ".format(x, y, answer))
