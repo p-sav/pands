eb0474a19c0279be91bd1c8a5efde68477e32c57
p-sav <g00438898@atu.ie>
2024-03-10 15:54:03 +0000

    week-3

---
+++ pands-mywork/Week-3/lab3.1.testtypes.py
@@ -0,0 +1,16 @@
+# lab3.1.testtypes.py 
+# program that tests variables 
+# author: p-sav
+
+i = 3
+fl = 3.5
+isa = True
+memo = 'How now Brown Cow'
+lots = []
+
+print ('variable {} is of type: {} and value:{}' .format('i', type(i), i))
+print ('variable {} is of type: {} and value:{}' .format('fl', type(fl), fl))
+print ('variable {} is of type: {} and value:{}' .format('isa', type(isa), isa))
+print ('variable {} is of type: {} and value:{}' .format('memo', type(memo), memo))
+print ('variable {} is of type: {} and value:{}' .format('lots', type(lots), lots))
+
