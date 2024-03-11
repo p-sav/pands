a35d7f100f0f3ae498caebcce5043d49f60eff50
p-sav <g00438898@atu.ie>
2024-03-11 22:39:16 +0000

    week-3

---
+++ pands-mywork/Week-3/lab3.2.absolute.py
@@ -0,0 +1,7 @@
+# absolute.py
+# program that gets the absolute value of a number
+# author: p-sav
+
+number = float(input("Enter a number:"))
+absolutevalue = abs(number)
+print('The absolute value of {} is {}' .format(number, absolutevalue))
---
+++ pands-mywork/Week-3/lab3.2.round.py
@@ -0,0 +1,8 @@
+# round.py
+# program thta takes in a float and outputs an integer
+# author: p-sav
+
+numbertoround = float(input("Enter a float number:"))
+roundednumber = round(numbertoround)
+print ('{} rounded is {}'.format(numbertoround,roundednumber))
+
